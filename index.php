75e569754e5ce8ea3b2400f2a0005c52008e372b
djkrule <75855893+djkrule@users.noreply.github.com>
2020-12-15 18:12:37 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -110,17 +110,6 @@ echo "<h3>No one is currently registered.</h3>";
 }
  else
 {
-U terminalu idite na putanju mape vaše aplikacije i upišite sljedeću naredbu:
-Sada možete u pregledniku otići na adresu http://localhost:8000/ kako biste testirali vašu
-aplikaciju.
-6. Objavljivanje vaše aplikacije
-Nakon što ste testirali vašu aplikaciju lokalno, možete ju objaviti na Web Apps-u korištenjem
-Git-a. Prvo ćete inicijalizirati svoj lokalni Git repozitorij ili klonirati novi repozitorij koji ste
-kreirali na GitHub-u, te potom objaviti aplikaciju.
-1. Kako biste povezali svoj lokalni Git repozitorij s Azure Web App-om, potrebno je prvo
-definirati deployment opciju na Azure upravljačkoj konzoli vašeg Web App servisa.
-Odaberite Local Git Repository ili GitHub ukoliko ste se odlučili koristiti GitHub
-udaljeni repozitorij.
 echo "Insert Failed";
 }
 }
